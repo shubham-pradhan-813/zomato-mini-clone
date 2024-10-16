f1530ee8e5de05b71d87207c32a9470b7a3f0480
Shubham Pradhan <144103003+shubham-pradhan-813@users.noreply.github.com>
2024-10-16 15:41:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const exploreBtn = document.getElementById('exploreBtn');
+    
+    exploreBtn.addEventListener('click', function() {
+        alert('Discover amazing food options near you!');
+    });
+});
